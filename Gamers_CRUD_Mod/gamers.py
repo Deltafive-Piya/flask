de481c13f68b1f4eb5d58bde394d2380ba7e6739
Deltafive-Piya <pierstoddard@gmail.com>
2023-07-18 23:05:07 -0400

    create-Gamers_CRUD_Mod

---
+++ Gamers_CRUD_Mod/gamers.py
@@ -0,0 +1,56 @@
+#import mysqlconnection
+from mysqlconnection import connectToMySQL
+#line this up with the schema in the database
+class Gamers:                                   
+    
+    #This was copied to populate the below, may need to adjust to match table
+    def __init__(self, data):
+        self.id = data['id']                    
+        self.f_name = data['f_name']
+        self.l_name = data['l_name']
+        self.tag = data['tag']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.clan_id = data['clan_id']
+
+    #CLASS METHOD
+    @classmethod
+    # get all the data from db and place it in our class USING A SELECT STATEMENT
+    def get_all(cls):                            
+        query = "SELECT * FROM gamers;"
+        #Input name of schema used; .query_db allows us to query db with 'query' stated 1 line above
+        results = connectToMySQL('gamers').query_db(query)      
+        
+        #This epmty list establishes a place to put our g
+        gamers = []                                             
+         # want to output as a list of 'gamer' objects; from dictionaries (sqldb) to objects(OOP language manipulateable)
+        for g in results:
+            #g for gamer, translate for the initiation-------------------------------further explain------------------------                                   
+            gamers.append( cls(g) )
+        #return back results of the work                            
+        return gamers
+    
+    #allow data input; for new_gamer.html
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO gamers (f_name, l_name, tag, clan_id) VALUES (%(f_name)s, %(l_name)s, %(tag)s, %(clan_id)s);"
+        
+        #returns as the new row id
+        result = connectToMySQL('gamers').query_db(query,data)
+        return result
+    
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM gamers WHERE id = %(id)s;"
+        result = connectToMySQL('gamers').query_db(query,data)
+        return cls(result[0])                                   #MUST TREAT THE OUTPUT AS A LIST, hence [0]
+    
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE gamers SET f_name=%(f_name)s, l_name=%(l_name)s, tag=%(tag)s, clan_id=%(clan_id)s, updated_at=NOW() WHERE id= %(id)s;"
+        return connectToMySQL('gamers').query_db(query, data)
+    
+    @classmethod
+    def destroy(cls,data):
+        query = "DELETE FROM gamers WHERE id = %(id)s;"
+        return connectToMySQL('gamers').query_db(query, data)
---
+++ Gamers_CRUD_Mod/server.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request, redirect
+
+from gamers import Gamers
+
+app = Flask(__name__)
+
+
+# HOME
+@app.route("/")  # redirecting index route to users page
+def index():  # Index has function to resdirect to route(/gamers)
+    return redirect("/gamers")
+
+
+# REDIRECT.HOME
+@app.route("/gamers")
+def gamers():  # gamers route has gamers() function
+    return render_template(
+        "gamers.html", gamers=Gamers.get_all()
+    )  # runs the gamers.py function get_all()
+
+
+# CREATE DISPLAY
+@app.route("/gamers/new")
+def new():  # new() will display html new_gamer.html
+    return render_template("new_gamer.html")
+
+
+# CREATE ACTION
+@app.route(
+    "/gamers/create", methods=["POST"]
+)  # requires a post method to post inputed data from html
+def create():
+    print(request.form)
+    Gamers.save(
+        request.form
+    )  # This line added to allow for input passing on new_gamers (where def save() is called)
+    return redirect("/gamers")
+
+# SHOW DISPLAY
+@app.route("/gamers/show/<int:id>")  # <int:id>
+def show(id):
+    data = {"id": id}
+    return render_template("show_gamer.html", gamers=Gamers.get_one(data))
+
+# UPDATE DISPLAY
+@app.route("/gamers/update/<int:id>")  # <int:id>
+def edit(id):
+    data = {"id": id}
+    return render_template("update_gamer.html", gamers=Gamers.get_one(data))
+
+
+# UPDATE ACTION
+@app.route("/gamers/update", methods=["POST"])  # METHODS NOT METHOD
+def update():
+    Gamers.update(request.form)
+    return redirect("/gamers")
+
+
+
+
+
+
+
+
+
+
+# DESTROY (ACTION ONLY; RELIANT ON HOME DISPLAY)
+@app.route("/gamers/destroy/<int:id>")
+def destroy(id):
+    data = {"id": id}
+    Gamers.destroy(data)  # The action to destroy lives in GamersDotPy
+    return redirect("/gamers")
+
+
+# LAUNCH
+if __name__ == "__main__":  # This black need to be last in file
+    app.run(host="localhost", port=5002, debug=True)
